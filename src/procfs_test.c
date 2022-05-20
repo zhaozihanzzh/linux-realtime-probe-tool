9221eecece704c0292e404a42cca17ad4dd6d7c6
叶景熙 <2586228167@qq.com>
2022-05-20 23:43:02 +0800

    Update procfs_test.c

--- src/procfs_test.c
+++ src/procfs_test.c
@@ -1,10 +1,10 @@
 #include <linux/module.h>
-#include<linux/string.h>
+#include <linux/string.h>
 #include <linux/init.h>
 #include <linux/proc_fs.h>
-#include<linux/uaccess.h>
-#include<linux/list.h>
-#include<linux/slab.h>
+#include <linux/uaccess.h>
+#include <linux/list.h>
+#include <linux/slab.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("QunChuWoLao");
@@ -14,22 +14,8 @@ struct data{
 	char number[16];
 };
 struct data data1;
-int overwrite_disck;
 
-static ssize_t proc_overwrite_switch_write(struct file *file,
-		const char __user *buf,
-		size_t size,
-		loff_t *offset);
-		
-static ssize_t proc_overwrite_switch_read(struct file *file,
-		char __user *buf,
-		size_t size,
-		loff_t *ppos);
-
-static struct file_operations s_st_overwrite_switch_fops = {
-	.write = proc_overwrite_switch_write,
-	.read = proc_overwrite_switch_read,
-};
+struct kmem_cache *my_cachep = NULL;
 
 static ssize_t proc_overwrite_switch_write(struct file *file,
 		const char __user *buf,
@@ -37,6 +23,7 @@ static ssize_t proc_overwrite_switch_write(struct file *file,
 		loff_t *offset)
 {
 	char tmp[16] = {0};
+	struct data *p = (struct data*)kmem_cache_zalloc(my_cachep, GFP_KERNEL);
 	
 	if(copy_from_user(&tmp, buf, size))
 		return -EFAULT;
@@ -45,16 +32,10 @@ static ssize_t proc_overwrite_switch_write(struct file *file,
 		tmp[strlen(tmp)] = 0x00;
 	if(tmp[strlen(tmp)-1] == '\n')
 		tmp[strlen(tmp)-1] = 0x00;
-		
-//	if(strcmp(tmp, "1") && strcmp(tmp, "0"))
-//		return -EINVAL;
-		
-	struct data *p = kmalloc(sizeof(struct data), GFP_KERNEL);
+			
 	strcpy(p->number, tmp);
-	list_add(&p->list, &data1.list);
+	list_add_tail(&p->list, &data1.list);
 		
-//	overwrite_disck = tmp[0] - '0';
-//	printk("GHTEST: overwrite disck == %d\n", overwrite_disck);
 	return size;
 }
 
@@ -66,23 +47,31 @@ static ssize_t proc_overwrite_switch_read(struct file *file,
 	char tmp_buf[16] = {0};
 	int len;
 	
-	struct data *pos;
-//	list_for_each_entry(pos, &data1.list, list){
-//		//pos->number;
-//	}
 	struct data *first;
 	first = list_first_entry(&data1.list, struct data, list);
 	list_del(&first->list);
 	printk("GHTEST: overwrite disck == %s\n", first->number);
 	len=snprintf(tmp_buf, 16, "%s", first->number);
-//	kfree(first);
+	kmem_cache_free(my_cachep, first);
 	return simple_read_from_buffer(buf, size, ppos, tmp_buf, len);
 }
 
+static struct file_operations s_st_overwrite_switch_fops = {
+	.write = proc_overwrite_switch_write,
+	.read = proc_overwrite_switch_read,
+};
+
 static int __init start_module(void)
 {
+	struct proc_dir_entry *s_pst_proc_overwrite_flag = NULL;
+	
     printk(KERN_ALERT "Module init.\n");
-    struct proc_dir_entry *s_pst_proc_overwrite_flag = NULL;
+    
+    my_cachep = kmem_cache_create("my_cache", sizeof(struct data), 0, SLAB_HWCACHE_ALIGN, NULL);
+    if(my_cachep == NULL ){
+    	printk("create my_cache failed!\n");
+    	return -ENOMEM;
+	}
     s_pst_proc_overwrite_flag = proc_create("overwrite_disck", 0744, NULL, &s_st_overwrite_switch_fops);
     if(s_pst_proc_overwrite_flag == NULL){
     	printk("create overwrite_disck failed\n");
@@ -93,6 +82,13 @@ static int __init start_module(void)
 }
 static void __exit exit_module(void)
 {
+	struct data *pos;
+	struct data *n;
+	list_for_each_entry_safe(pos, n, &data1.list, list){
+		list_del(&pos->list);
+		kmem_cache_free(my_cachep, pos);
+	}
+	kmem_cache_destroy(my_cachep);
 	remove_proc_entry("overwrite_disck", NULL);
     printk(KERN_ALERT "Module exit.\n");
 }
